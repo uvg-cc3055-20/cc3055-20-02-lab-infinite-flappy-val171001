524061559dc8a921c0be15df257e1d58de0a7bc8
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-01-24 08:21:44 -0600

    - Movimiento infinifto de la escena

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+	public float scrolLingSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Move scene forward
+		transform.Translate (Vector3.left * scrolLingSpeed * Time.deltaTime);
+
+		// Infinite scene
+		if (transform.position.x < -20.4f) {
+			transform.position = new Vector3 (20.4f, transform.position.y, transform.position.z);
+		}
+		
+	}
+}
